eb44d12cec7903c01365f241904c69818ac2db5d
aprendiendoconroberto-lab <aprendiendoconroberto@gmail.com>
2025-12-17 17:06:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
+def leer_rutas_activas():
+    rutas = []
+    with open("data/rutas.csv", newline="", encoding="utf-8") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            if fila["activo"].lower() == "true":
+                rutas.append(fila)
+    return rutas
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    rutas = leer_rutas_activas()
+    return render_template("index.html", rutas=rutas)
 
 if __name__ == "__main__":
     app.run()
-
